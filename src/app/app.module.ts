3578f8e38a9de138b53f3fe3aaaba34b5a4071d2
meugz <mzeugenia@outlook.es>
2022-05-07 12:36:18 -0300

    actualizo rama develop con maqueta de componentes en codigo html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { HeaderIntroComponent } from './componentes/header-intro/header-intro.component';
+import { SocialMediaComponent } from './componentes/social-media/social-media.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { ExperienceComponent } from './componentes/experience/experience.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { InterestComponent } from './componentes/interest/interest.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    HeaderIntroComponent,
+    SocialMediaComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestComponent,
+    ContactComponent,
+    FooterComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/header-intro/header-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-intro',
+  templateUrl: './header-intro.component.html',
+  styleUrls: ['./header-intro.component.css']
+})
+export class HeaderIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
